68f0a6ff2cbed4b650c4b2424637f6150232d119
=Anthony Rodriguez <AnthonyRodriguez.itt@gmail.com>
2015-04-28 17:02:16 -0400

    Refactoring view for Radio Proezas

--- radiostreaming/ViewController.swift
+++ radiostreaming/ViewController.swift
@@ -12,9 +12,12 @@ class ViewController: UIViewController {
 
     let player = PlayerService()
     
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.stopButton.hidden = true
         
         
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,13 +27,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func toggleButton(me: UIButton, next: UIButton){
+        me.hidden = !me.hidden
+        next.hidden = !next.hidden
+    }
 
     @IBAction func playPressed(sender: UIButton) {
         self.player.play()
+        self.toggleButton(sender, next: self.stopButton)
     }
 
     @IBAction func stopPressed(sender: UIButton) {
         self.player.stop()
-    }
+        self.toggleButton(sender, next: self.playButton)    }
 }
 
